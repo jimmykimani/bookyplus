95def61b4a0ec2a76b00834c1bd729c4b31fab9c
jimmykimani <jimmykkimani@gmail.com>
2017-02-05 22:12:18 +0300

    Modify url validation

--- thermo/forms.py
+++ thermo/forms.py
@@ -1,12 +1,22 @@
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email, Regexp, EqualTo,url
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo,url
 from wtforms import ValidationError
 from flask_wtf.html5 import URLField
 
 
-class BookmarkForm(FlaskForm):
-    url = URLField('url', validators=[Required(), url()])
-    description = StringField('Description', validators=[Required()] )
+class BookmarkForm(Form):
+    url = URLField('url', validators=[DataRequired(), url()])
+    description = StringField('Description', validators=[DataRequired()] )
     
 
+def validate(self):
+    if not self.url.data.startwith('http://') or \
+            self.url.data.startswith('http://'):
+            self.url.data = 'http://'+ self.url.data
+
+    if not Form.validated(self):
+        return False
+    if not self.description.data:
+        self.description.data = self.url.data
+    return True
